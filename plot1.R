79fb2d8cb7cf84f3d9a40000d909e4cfaa41b186
Bill Kayser <bkayser@newrelic.com>
2015-09-12 12:09:35 -0700

    Updates, improved loading

--- plot1.R
+++ plot1.R
@@ -1,23 +1,27 @@
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # Read the data skipping the rows outside the time range of interest
 #
+library(dplyr)
+
+# The whole table is read into about 133MB of memory which is fine.
 data <- read.table('../household_power_consumption.txt', 
                    sep=";", 
-                   skip=66637, # skip all data up to feb 2007
-                   nrows=2880, # read in feb 1,2 only
                    na.strings="?", 
+                   header=T,
+                   colClasses = c('factor','factor','numeric','numeric','numeric','numeric','numeric','numeric','numeric'),
                    col.names=c('Date','Time','GAP', 'GRP', 'Voltage', 'GI', 
                                'Sub_metering_1',
                                'Sub_metering_2',
                                'Sub_metering_3'),
-                   stringsAsFactors=c(TRUE,FALSE))
+                   stringsAsFactors=c(TRUE,FALSE)) %>%
+        subset(Date == '1/2/2007' | Date == '2/2/2007')
 
 # Create a new column for the timestamp by parsing the date and time
 data$Timestamp = strptime(paste(data$Date, data$Time), '%d/%m/%Y %H:%M:%S')
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # Create the plot on a PNG device
-png(filename='plot1.png')
+png(filename='plot1.png', width=480, height=480)
 
 hist(data$GAP, 
      col='red', 
--- plot3.R
+++ plot3.R
@@ -1,23 +1,26 @@
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # Read the data skipping the rows outside the time range of interest
 #
+library(dplyr)
+
 data <- read.table('../household_power_consumption.txt', 
                    sep=";", 
-                   skip=66637, # skip all data up to feb 2007
-                   nrows=2880, # read in feb 1,2 only
                    na.strings="?", 
+                   header=T,
+                   colClasses = c('factor','factor','numeric','numeric','numeric','numeric','numeric','numeric','numeric'),
                    col.names=c('Date','Time','GAP', 'GRP', 'Voltage', 'GI', 
                                'Sub_metering_1',
                                'Sub_metering_2',
                                'Sub_metering_3'),
-                   stringsAsFactors=c(TRUE,FALSE))
+                   stringsAsFactors=c(TRUE,FALSE)) %>%
+    subset(Date == '1/2/2007' | Date == '2/2/2007')
 
 # Create a new column for the timestamp by parsing the date and time
 data$Timestamp = strptime(paste(data$Date, data$Time), '%d/%m/%Y %H:%M:%S')
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # Create the plot on a PNG device
-png(filename='plot3.png')
+png(filename='plot3.png', width=480, height=480)
 
 plot(data$Timestamp, 
      data$Sub_metering_1,
